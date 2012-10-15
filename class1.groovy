b61fa96c9100188e1cc2de649a7004e97f8d9b00
unknown <melnas01@cspc048.dcs.bbk.ac.uk>
2012-10-15 18:04:59 +0100

    class to play around with student.father.name

---
+++ class1.groovy
@@ -0,0 +1,24 @@
+class Person {
+String name;
+int age;
+Person father;
+Person mother;
+}
+
+Person john = new Person();
+john.name = "John Smith";
+john.age = 35;
+Person mary = new Person();
+mary.name = "Mary Smith";
+mary.age = 32;
+Person student = new Person();
+student.name = "John Smith, Jr.";
+student.age = 5;
+student.father = john
+student.father.name="John"
+student.mother = mary
+student.mother.name = "mary"
+println "TEACHER: How old are you, " + student.name + "?"
+println "LITTLE JOHN: I am " + student.age + " years old, sir.";
+println "TEACHER: Who is your mother?"
+println "LITTLE JOHN: " + student.mother.name + ", sir.";
